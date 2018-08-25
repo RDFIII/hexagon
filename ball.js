b38d26d60b83d576d2ea07351c393110369506d8
RDF <foulkesr@gmail.com>
2018-08-25 15:40:58 -0400

    ball now correctly interacts with sides of window

--- ball.js
+++ ball.js
@@ -17,19 +17,19 @@ class Ball {
   move(){
       this.x += this.xVelocity;
       this.y += this.yVelocity;
-      // this.bounce();
+      this.bounce();
     }
 
-  // bounce(){
-  //   if(this.x - this.r <= 0 || this.x + this.r >= width){
-  //     this.xVelocity *= -1;
-  //     // sound();
-  //     console.log("contact");
-  //   }
-  //   if(this.y - this.r <= 0 || this.y + this.r >= height){
-  //     this.yVelocity *= -1;
-  //     // sound();
-  //     console.log("contact");
-  //   }
-  // }
+    bounce(){
+    if(this.x - this.r <= 0 || this.x + this.r >= windowWidth){
+      this.xVelocity *= -1;
+      // sound();
+      console.log("contact");
+    }
+    if(this.y - this.r <= 0 || this.y + this.r >= windowHeight){
+      this.yVelocity *= -1;
+      // sound();
+      console.log("contact");
+    }
+  }
 }
--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
-	ball1 = new Ball(0, 0, 40, -2.5, 2);
+	ball1 = new Ball(width/2, height/2, 40, -2.5, 2);
 	poly = new Polygondwana(0, 0, windowHeight/2.8, 8);
 	// ball1 = new Ball(windowWidth/2, windowHeight/2, 40, 2, 3);
 
@@ -9,12 +9,11 @@ function setup() {
 function draw() {
   background(14, 63, 142);
 
-	  translate(windowWidth/2, windowHeight/2);
-
 	push();
+	translate(windowWidth/2, windowHeight/2);
   poly.show();
 	pop();
-	
+
 	ball1.show();
 	ball1.move();
 
